4d82d1c546a5d71eb80399d6cce7a0c2408a4512
Shanthi Karunakaran <shanthi.arun@gmail.com>
2016-09-24 08:27:19 -0700

    removed comments

--- ShanthiK-Portfolio/data/quotes.js
+++ ShanthiK-Portfolio/data/quotes.js
@@ -1,16 +1,14 @@
-{
-   "quotes":[
+quotes:[
       {
-         "quote":"Don't cry because it's over, smile because it happened.",
-         "author":"Dr. Seuss"
+         quote:"Don't cry because it's over, smile because it happened.",
+         author:"Dr. Seuss"
       },
       {
-         "quote":"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
-         "author":"Marilyn Monroe"
+         quote:"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+         author:"Marilyn Monroe"
       },
       {
-         "quote":"Be yourself; everyone else is already taken.",
-         "author":"Oscar Wilde"
+         quote:"Be yourself; everyone else is already taken.",
+         author:"Oscar Wilde"
       }
-   ]
-}
+]
