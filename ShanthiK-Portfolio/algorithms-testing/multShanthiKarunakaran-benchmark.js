8d059a042b4ea8cbaf9c4cdd8615542059115296
Shanthi Karunakaran <shanthi.arun@gmail.com>
2016-10-14 12:43:34 -0700

    added test suites for new js file

--- ShanthiK-Portfolio/algorithms-testing/multShanthiKarunakaran-benchmark.js
+++ ShanthiK-Portfolio/algorithms-testing/multShanthiKarunakaran-benchmark.js
@@ -1,11 +1,15 @@
 var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
 var multShanthiKarunakaran = require('./multShanthiKarunakaran.js');
+var multShanthiKarunakaran1 = require('./multShanthiKarunakaran1.js');
 
 // add tests
 suite
   .add('multShanthiKarunakaran Reduce 10000', () => {
     multShanthiKarunakaran(10000);
+  })
+  .add('multShanthiKarunakaran Reduce 10000', () => {
+    multShanthiKarunakaran1(10000);
   })
   .on('cycle', (event) => {
     console.log(String(event.target));
--- ShanthiK-Portfolio/algorithms-testing/multShanthiKarunakaran-test.js
+++ ShanthiK-Portfolio/algorithms-testing/multShanthiKarunakaran-test.js
@@ -1,5 +1,6 @@
 const should = require('chai').should();
 const basic = require('./multShanthiKarunakaran');
+const basic1 = require('./multShanthiKarunakaran1');
 
 describe('basic', () => {
   it('should return 23 when passed 10', () => {
@@ -16,5 +17,23 @@ describe('basic', () => {
   });
   it('should return 486804150 when passed 45678', () => {
     basic(45678).should.equal(486804150);
+  });
+});
+
+describe('basic1', () => {
+  it('should return 23 when passed 10', () => {
+    basic1(10).should.equal(23);
+  });
+  it('should return 78 when passed 20', () => {
+    basic1(20).should.equal(78);
+  });
+  it('should return 2318 when passed 100', () => {
+    basic1(100).should.equal(2318);
+  });
+  it('should return 23331668 when passed 10000', () => {
+    basic1(10000).should.equal(23331668);
+  });
+  it('should return 486804150 when passed 45678', () => {
+    basic1(45678).should.equal(486804150);
   });
 });
